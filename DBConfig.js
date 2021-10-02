8ccc52b602a525f900697bc35b90bd1c25332e88
bihtyu <bihtyu@163.com>
2021-10-02 23:13:43 +0800

    add mongodb

---
+++ DBConfig.js
@@ -0,0 +1,3 @@
+export const CONNECTION_STRING = "mongodb+srv://bihtyu:afvbLDbUTbN9rxm@shorturlonmongodb.fjepg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+export const DB_NAME = "SHORT_URL"
--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,20 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+const { MongoClient } = require('mongodb');
+import { MONGODB_CONNECTION_STRING, DB_NAME } from '../DBConfig.js' 
+
 export default async (req: VercelRequest, res: VercelResponse): Promise<any> => {
-  console.log(req)
-  console.log(res)
-  return res.send({ result: 'you got me!' })
+  const client = new MongoClient(MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  const collection = await client.connect(err => {
+    const collection = client.db(DB_NAME).collection("links")
+    return collection
+  })
+
+  const result = collection.find().toArray()
+
+  client.close()
+
+  // return res.send({ code: 20000, result: 'you got me!' })
+  
+  res.status(200).json(result)
 }
