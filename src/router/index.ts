a7b7031d4370ac81acad8bed7fb2240787b56fee
bihtyu <bihtyu@163.com>
2021-10-06 17:43:22 +0800

    add front end framework: vue3

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home/index.vue')
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
