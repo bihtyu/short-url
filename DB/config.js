51c592a88a97268ea61f6596070d4a383c6dc49a
bihtyu <bihtyu@163.com>
2021-10-25 07:34:35 +0800

    Update config.js

--- DB/config.js
+++ DB/config.js
@@ -1,3 +1,3 @@
-export const MONGODB_CONNECTION_STRING = "mongodb+srv://bihtyu:afvbLDbUTbN9rxm@shorturlonmongodb.fjepg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const MONGODB_CONNECTION_STRING = "xxx"
 
-export const DB_NAME = "SHORT_URL"
+export const DB_NAME = "xxx"
